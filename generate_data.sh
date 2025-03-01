7108437618e8805c7a0524b54cc0835e0894f7b6
sinhSlumbering <muztoba.sinha@gmail.com>
2025-03-01 17:43:21 +0600

    Some more changes

--- generate_data.sh
+++ generate_data.sh
@@ -32,7 +32,7 @@ for nodes in "${!node_samples[@]}"; do
         --min_nodes "$nodes" \
         --max_nodes "$nodes" \
         --num_samples "$num_samples" \
-        --filename "${base_filename}${nodes}-${nodes}_train/train.txt" \
+        --filename "${base_filename}${nodes}-${nodes}_train/${nodes}-${nodes}_train.txt" \
         --node_dim 2 \
         --seed "$seed"
 
@@ -42,7 +42,7 @@ for nodes in "${!node_samples[@]}"; do
         --min_nodes "$nodes" \
         --max_nodes "$nodes" \
         --num_samples "$val_test_samples" \
-        --filename "${base_filename}${nodes}-${nodes}_val/val.txt" \
+        --filename "${base_filename}${nodes}-${nodes}_val/${nodes}-${nodes}_val.txt" \
         --node_dim 2 \
         --seed "$val_seed"
 
@@ -52,11 +52,13 @@ for nodes in "${!node_samples[@]}"; do
         --min_nodes "$nodes" \
         --max_nodes "$nodes" \
         --num_samples "$val_test_samples" \
-        --filename "${base_filename}${nodes}-${nodes}_test/test.txt" \
+        --filename "${base_filename}${nodes}-${nodes}_test/${nodes}-${nodes}_test.txt" \
         --node_dim 2 \
         --seed "$test_seed"
 
     echo "Generated data for ${nodes} nodes."
 done
+rm *.res
+rm *.sol
 
 echo "Finished generating all datasets."
