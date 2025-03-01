1cbd64d1ef35fd27efb357add84be38c9fa919df
sinhSlumbering <muztoba.sinha@gmail.com>
2025-03-02 00:26:47 +0600

    Data Pipeline Changed Slightly 	modified:   generate_data.sh 	new file:   pickle_data.sh

--- generate_data.sh
+++ generate_data.sh
@@ -32,7 +32,7 @@ for nodes in "${!node_samples[@]}"; do
         --min_nodes "$nodes" \
         --max_nodes "$nodes" \
         --num_samples "$num_samples" \
-        --filename "${base_filename}${nodes}-${nodes}_train/train.txt" \
+        --filename "${base_filename}${nodes}-${nodes}_train/tsp${nodes}-${nodes}_train.txt" \
         --node_dim 2 \
         --seed "$seed"
 
@@ -42,7 +42,7 @@ for nodes in "${!node_samples[@]}"; do
         --min_nodes "$nodes" \
         --max_nodes "$nodes" \
         --num_samples "$val_test_samples" \
-        --filename "${base_filename}${nodes}-${nodes}_val/val.txt" \
+        --filename "${base_filename}${nodes}-${nodes}_val/tsp${nodes}-${nodes}_val.txt" \
         --node_dim 2 \
         --seed "$val_seed"
 
@@ -52,11 +52,13 @@ for nodes in "${!node_samples[@]}"; do
         --min_nodes "$nodes" \
         --max_nodes "$nodes" \
         --num_samples "$val_test_samples" \
-        --filename "${base_filename}${nodes}-${nodes}_test/test.txt" \
+        --filename "${base_filename}${nodes}-${nodes}_test/tsp${nodes}-${nodes}_test.txt" \
         --node_dim 2 \
         --seed "$test_seed"
 
     echo "Generated data for ${nodes} nodes."
 done
+rm *.res
+rm *.sol
 
 echo "Finished generating all datasets."
---
+++ pickle_data.sh
@@ -0,0 +1,5 @@
+cp tsp100-100_test/tsp100-100_test.txt data/
+cp tsp100-100_train/tsp100-100_train.txt data/
+cp tsp100-100_val/tsp100-100_val.txt data/
+
+python data/TSP/TSP_Prepare.py
